325e614b9d58f4ea239137a6e512e2078db47212
weenke <47457773+weenke@users.noreply.github.com>
2023-03-06 01:29:01 +0200

    Update shapeanalysis.py
    accumulated changes added

--- shapeanalysis.py
+++ shapeanalysis.py
@@ -109,11 +109,13 @@ class ShapeAnalysis:
         min_intensity = np.min(self.image_array[np.where(self.apply_mask_circle_fit() == 255)])
         return min_intensity
 
+        
+
 
     def roi_array_w_parameters(self):
         radius, center = self.radius, self.getCentroid()
-        start = [self.getCentroid()[1]-radius, self.getCentroid()[1]+radius]
-        fin = [self.getCentroid()[0]-radius, self.getCentroid()[0]+radius]
+        start = [center[1]-radius, center[1]+radius]
+        fin = [center[0]-radius, center[0]+radius]
         roi = self.image_array[start[0]:start[1],fin[0]:fin[1]]
         return roi
 
@@ -144,7 +146,7 @@ class NormalizedImageAnalysis:
         background_mean_value = self.mean_infinite_background_value    
         outside_intensity = np.ones(self.roi_shape)*background_mean_value
         #print(outside_intensity)
-        minimal_instide_intensity = np.ones(self.roi_shape)*self.roi_min_intensity
+        minimal_instide_intensity = np.ones(self.roi_shape)*self.min_point_fromdata()
         #print(minimal_instide_intensity)
         #eq_normalazation = lambda intensity: round(((intensity-minimal_instide_intensity)/(outside_intensity-minimal_instide_intensity)),2)
         #img_normalized_concentration = np.vectorize(eq_normalazation)
@@ -164,13 +166,29 @@ class NormalizedImageAnalysis:
 
     def mask_roi(self):
         normal_image = self.imageNormalization()
-        return_image = (normal_image > 0.8).astype(int)
+        return_image = (normal_image > 0.95     ).astype(int)
         return return_image
 
     def draw_cricle_set_radius(self, set_radius):
         im_copy = self.roi_image_array.copy()
         cv.circle(im_copy, self.main_shape_center, set_radius, 255, 1)
         return im_copy
+
+    def min_point_fromdata(self):
+        mean_y_values_array = np.array([np.mean(self.roi_image_array[index, :]) for index in range(len(self.roi_image_array))])
+        mean_x_values_array = np.array([np.mean(self.roi_image_array[:, index]) for index in range(len(self.roi_image_array))])
+        min_point_y = np.argmin(mean_y_values_array)
+        min_point_x = np.argmin(mean_x_values_array)
+        return self.roi_image_array[min_point_y, min_point_x]
+    
+    def radius_roi(self, radius, min_ceter):
+        start = [min_ceter[0]-int(radius), min_ceter[0]+int(radius)]
+        print(start)
+        fin = [min_ceter[1]-int(radius), min_ceter[1]+int(radius)]
+        print(fin)
+        roi = self.roi_image_array[start[0]:start[1],fin[0]:fin[1]]
+        return roi
+
 
 class IntensityRecoveryAnalysis(ShapeAnalysis):
 
@@ -189,8 +207,8 @@ class IntensityRecoveryAnalysis(ShapeAnalysis):
 class MinorShapeAnalysis:
 
 
-    def _binary_gaussian_mixture_outter_shape(self):
-        n_comp = 3
+    def _binary_gaussian_mixture_outter_shape(self, comps_num=3):
+        n_comp = comps_num
         labels = GaussianMixture(n_components=n_comp).fit_predict(self.processed_image.ravel().reshape(-1, 1))
         labels_to_image = np.array((labels.reshape(self.image_shape)), dtype='uint8')
         label_means = [(i, np.mean(self.image_array[np.where(labels_to_image==i)])) for i in range(n_comp)]
@@ -211,10 +229,10 @@ class MinorShapeAnalysis:
 
     def _image_preprocessing(self, return_image=False):
         
-        #img_historgram_correction = rescale_intensity(equalize_adapthist(self.image_array), out_range='uint8')
-        #img_median_filtered = median_filter(img_historgram_correction, (10,10))
-        img_bilating = cv.bilateralFilter(self.image_array, 5, 40, 10)
-        img_erosion = cv.erode(img_bilating, self.kernel, iterations=2)
+        img_historgram_correction = rescale_intensity(equalize_adapthist(self.image_array), out_range='uint8')
+        img_median_filtered = median_filter(img_historgram_correction, (5,5))
+        img_bilating = cv.bilateralFilter(img_median_filtered, 10, 25, 25)
+        img_erosion = cv.erode(img_bilating, self.kernel, iterations=1)
         if return_image == False:
             return img_erosion
         else:    
@@ -261,6 +279,14 @@ class MinorShapeAnalysis:
             if h_list[-1] != -1:
                 parent_contour = h_list[-1]
         return parent_contour
+    
+    def containing_contour_biggest_get(self):
+        #contained_contours = self.contained_cnoturs()
+        contour_data = self.contour_on_labeled_image()
+        for index, h_list in enumerate(contour_data[1][0]):
+            if h_list[-1] != -1:
+                parent_contour = h_list[-1]
+        return contour_data[0][parent_contour]
 
     def containig_contours(self):
         parent_contour = None
@@ -364,13 +390,18 @@ class IntensityRecoveryAnalysis_v2(MinorShapeAnalysis):
     
     def __init__(self, image):
         super().__init__(image)
+        self.main_shape_center = (int(self.image_shape[0]/2),int(self.image_shape[1]/2))
+
 
     def defined_roi_mean_intensity(self, set_radius):
         mask = np.zeros(self.image_shape)
-        cv.circle(mask, self.getCentroid(), set_radius, 255, cv.FILLED)
+        cv.circle(mask, (self.main_shape_center[0], self.main_shape_center[1]), set_radius, 255, cv.FILLED)
         mean_value = np.mean(self.image_array[np.where(mask==255)])
-        complement_mask = self.complement_mask()
-        photobleacing_correction = np.mean(self.image_array[np.where(complement_mask==255)])
-        return [mean_value, photobleacing_correction]
+        #complement_mask = self.complement_mask()
+        #photobleacing_correction = np.mean(self.image_array[np.where(complement_mask==255)])
+        return mean_value
 
 #def analysis_worker():
+
+if __name__ == '__main__':
+    print('not as module')
